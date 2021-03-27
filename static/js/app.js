097dd70087535e684c5189d2e8005ac59d7d98e0
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-03-27 00:44:40 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
-// from data.js
-var tableData = data;
+// variable for data from data.js
+let tableData = data;
 
-// YOUR CODE HERE!
+// reference table body tag
+let tbody = d3.select("tbody");
+
+// arrow function to loop through tableData
+const ufoSightings = (item) => {
+    // append tr tag to table to add rows
+    let row =  tbody.append("tr");
+    
+    // arrow function to loop through keys and values
+    const ufoDetails = ([key, value]) => {
+        // append cells to rows and add text
+        let cell = row.append("td").text(value)
+    }
+    // call ufoDetails functions for each item entry
+    Object.entries(item).forEach(ufoDetails)
+};
+
+// call function on tableData to populate table
+tableData.forEach(ufoSightings);
